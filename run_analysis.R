936792e80f858ea47a4545237aa6860d9a5ccb19
Mayank Tyagi <maayanktyagi@gmail.com>
2016-11-07 00:32:44 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,5 @@
-library(dplyr)
 library(plyr)
-
+library(dplyr)
 if(!file.exists("datafile.zip")) {
   Url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
   download.file(Url, destfile = "datafile.zip",method = "curl")
@@ -22,27 +21,31 @@ train_sensor <- cbind(train_initial,y_training)
 test_sensor <- cbind(test_initial,y_test)
 
 sensor_data <- rbind(train_sensor,test_sensor)
-
+activity_labels <-dplyr::rename(activity_labels, Activity_ID = V1,Activity_name = V2)
 
 features <- read.table("UCI HAR Dataset/features.txt")
 features[,2] <- as.character(features[,2])
 activity_labels[,2] <- as.character(activity_labels[,2])
 
 sensor_labels_row <- rbind(features, c(562,"Subject")) 
-sensor_labels <- rbind(sensor_labels_row, c(563,"A"))[,2]
+sensor_labels <- rbind(sensor_labels_row, c(563,"Activity_ID"))[,2]
 
 names(sensor_data) <- sensor_labels
 
 mean_data <- sensor_data[,grepl("mean",names(sensor_data))]
 
-std_data <- sensor_data[,grepl("mean|std|Subject|ActivityId", names(sensor_data))]
+std_data <- sensor_data[,grepl("mean|std|Subject|Activity_ID", names(sensor_data))]
 
 
-merged_data <- join(std_data,activity_labels , by = "ActivityId", match = "first") 
+merged_data <- join(std_data, activity_labels , by = "Activity_ID", match = "first") 
 
 
 merged_data <- merged_data[,-1]
 
+
+tidy_dataset <- ddply(merged_data,c("Subject",Activity = "Activity_name"), function(x) colMeans(x[ 1:66],na.rm= FALSE))
+
+write.table(tidy_dataset,"tidy_dataset.txt")
 
 
 
